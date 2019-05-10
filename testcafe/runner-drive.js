a16ba957b70deef76abafce794a18d11c7fcfcd0
CozyKourai <julie.clement@cozycloud.cc>
2019-05-10 14:01:34 +0200

    All tests + parallel + no cdpPort

--- testcafe/runner-drive.js
+++ testcafe/runner-drive.js
@@ -21,13 +21,12 @@ async function runRunner() {
       //Tests !
       //'testcafe/tests/drive/classification_scenario.js',
       'testcafe/tests/drive/navigation.js',
-      'testcafe/tests/drive/folder_sharing_scenario.js'
-      //  'testcafe/tests/drive/file_sharing_scenario.js',
-      //  'testcafe/tests/drive/viewer-feature.js',
-      //'testcafe/tests/drive/public-viewer-feature.js'
+      'testcafe/tests/drive/folder_sharing_scenario.js',
+      'testcafe/tests/drive/file_sharing_scenario.js',
+      'testcafe/tests/drive/viewer-feature.js',
+      'testcafe/tests/drive/public-viewer-feature.js'
     ])
-    //emulation:cdpPort=9222 is used to set the download folder in headless mode
-    .browsers(['chrome:headless:emulation:cdpPort=9222 --start-maximized'])
+    .browsers(['chrome:headless:emulation --start-maximized'])
 
     .screenshots(
       'reports/',
--- testcafe/runner-photos.js
+++ testcafe/runner-photos.js
@@ -20,15 +20,14 @@ async function runRunner() {
       'testcafe/tests/photos/photos_start_upload_photos.js',
 
       'testcafe/tests/photos/photos_crud.js',
-      //  'testcafe/tests/photos/create_full_album_scenario.js',
-      //'testcafe/tests/photos/create_empty_album_scenario.js',
-      //'testcafe/tests/photos/album_sharing_scenario.js',
+      'testcafe/tests/photos/create_full_album_scenario.js',
+      'testcafe/tests/photos/create_empty_album_scenario.js',
+      'testcafe/tests/photos/album_sharing_scenario.js',
 
       //Scenario that just delete photos, so we don't need to do it in every test.
       'testcafe/tests/photos/photos_end_delete_all_data.js'
     ])
-    //emulation:cdpPort=9222 is used to set the download folder in headless mode
-    .browsers(['chrome:headless:emulation:cdpPort=9222 --start-maximized'])
+    .browsers(['chrome:headless:emulation --start-maximized'])
 
     .screenshots(
       'reports/',
