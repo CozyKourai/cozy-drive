801821e14418f8578538590262d2103af03a4039
CozyKourai <julie.clement@cozycloud.cc>
2019-05-10 08:23:33 +0200

    less tests

--- testcafe/runner-drive.js
+++ testcafe/runner-drive.js
@@ -10,9 +10,9 @@ async function runRunner() {
 
       //Tests !
       'testcafe/tests/drive/navigation.js',
-      'testcafe/tests/drive/folder_sharing_scenario.js',
-      'testcafe/tests/drive/file_sharing_scenario.js',
-      'testcafe/tests/drive/viewer-feature.js'
+      'testcafe/tests/drive/folder_sharing_scenario.js'
+      //  'testcafe/tests/drive/file_sharing_scenario.js',
+      //'testcafe/tests/drive/viewer-feature.js'
     ])
     //emulation:cdpPort=9222 is used to set the download folder in headless mode
     .browsers(['chrome:headless:emulation:cdpPort=9222 --start-maximized'])
